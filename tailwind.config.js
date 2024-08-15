b07be3ae0750c16f520911502cfc3f67428d011e
mahmoud <bewpspecialist@gmail.com>
2024-08-15 11:57:19 +0300

    d

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#3b82f6',
+        'green': '#28c76f',
+        'red': '#ff4c51',
+        'red-light':'#ffebee',
+        'orange': '#ff9f43',
+        'white': '#fff',
+        'gray-100': '#f3f4f6', // gray-100
+        'gray-50': '#f9fafb', // gray-50
+      },
+    },
+  },
+  plugins: [],
+}
+
