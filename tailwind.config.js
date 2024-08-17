283326c8b0fc34e6b5a1c2b224eb90a7fcab7a11
mahmoud <bewpspecialist@gmail.com>
2024-08-17 16:37:13 +0300

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'blue': '#3b82f6',
-        'green': '#28c76f',
-        'red': '#ff4c51',
-        'red-light':'#ffebee',
-        'orange': '#ff9f43',
-        'orange-light':'#ffe0b2',
-        'white': '#fff',
-        'gray-100': '#f3f4f6', // gray-100
-        'gray-50': '#f9fafb', // gray-50
+        'leads': '#3b82f6', // blue
+        'follow-up': '#ffeb3b', // Yellow
+        'follow-light': '#fff9c4', // Yellow-L
+        'intersted': '#ff9f43', // orange
+        'intersted-light':'#fff3e0', // orange-L
+        'trail':'#675DD8', // purp
+        'trail-light':'#f8bbd0', // pink-L
+        'successful': '#28c76f', // green
+        'successful-light': '#c8e6c9', // green-L
+        'error': '#ff4c51', // red
+        'error-light':'#ffebee', // red-L
+        'not': '#737682', // dark gray
+        'no-answer': '#eeeeee', // light gray
       },
     },
   },
